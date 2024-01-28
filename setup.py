8edf3f3d18e865c784c91aadadd217db2fb340cc
eddspark <111830791+eddspark@users.noreply.github.com>
2024-01-29 10:16:57 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     version=__version__,
     author='Matthijs van der Zee',
     setup_requires=["numpy",'pandas', 'scipy','wheel'],
-    packages=find_packages('.'), 
     build_requires=["numpy",'pandas', 'scipy','wheel'],
     py_modules=['WeightedCorr'], 
     install_requires=['numpy', 'pandas', 'scipy','wheel'],
